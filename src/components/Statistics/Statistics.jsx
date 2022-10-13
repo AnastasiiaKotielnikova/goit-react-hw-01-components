26642cc92753a7447e236d5c801fce07b98d4d04
AnastasiiaKotielnikova <akotelnikova1993@gmail.com>
2022-10-13 14:34:33 +0300

    add statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import styles from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats }) => {
+  <section className={styles.statistics}>
+    {title.length > 0 ? <h2 className={styles.title}>{title}</h2> : ''}
+
+    <ul className={styles.statList}>
+      {stats.map(item => (
+        <li className="item" key={item.label}>
+          <span className={styles.label}>{item.label}</span>
+          <span className={styles.percentage}>{item.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>;
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    }).isRequired
+  ).isRequired,
+};
+
+export default Statistics;
